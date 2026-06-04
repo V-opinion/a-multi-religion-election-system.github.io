68cb003fdd59ad28ba25bd0ffad23eec0c691b51
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-06-05 00:47:03 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -5,18 +5,18 @@ window.local_nivoslider_1_data = [
 },
 {
  caption: "Multireligionvalsystem.",
- image: "https://a-multi-religion-election-system.multireligionvalsystem.eu.org/1409.png"
+ image: "https://mres.multireligionvalsystem.eu.org/1409.png"
 },
 {
  caption: "political practices in exile ",
- image: "https://a-multi-religion-election-system.multireligionvalsystem.eu.org/1410.png"
+ image: "https://mres.multireligionvalsystem.eu.org/1410.png"
 },
 {
  caption: "F\u00F6r en h\u00E5llbar politik som leder  till en h\u00E5llbar utveckling. ",
- image: "https://a-multi-religion-election-system.multireligionvalsystem.eu.org/1411.png"
+ image: "https://mres.multireligionvalsystem.eu.org/1411.png"
 },
 {
  caption: "Den 77 \u00E5r l\u00E5nga konflikten mellan Israel och Palestina\u000A.",
- image: "https://a-multi-religion-election-system.multireligionvalsystem.eu.org/1412.png"
+ image: "https://mres.multireligionvalsystem.eu.org/1412.png"
 }
 ];
